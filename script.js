d42900aec8d8494cf82704a555bb47a3a05169ea
melievkh <khushnudmeliev21@gmail.com>
2022-09-01 21:51:18 +0500

    speechToText

---
+++ script.js
@@ -0,0 +1,12 @@
+var btn = document.getElementById("btn");
+btn.addEventListener("click", (ea) => {
+  btn.classList.add("fade");
+
+  let recognition = new webkitSpeechRecognition();
+  recognition.lang = "en-GB";
+  recognition.addEventListener("result", (e) => {
+    document.getElementById("text").value = e.results[0][0].transcript;
+    btn.classList.remove("fade");
+  });
+  recognition.start();
+});
